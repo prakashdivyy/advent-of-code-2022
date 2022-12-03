be338347f3884f2909123379ad0bd3a55d602907
Prakash Divy <prakashdivyy@gmail.com>
2022-12-03 17:59:01 +0700

    Finish day 3 part 2

--- 03/solve.sh
+++ 03/solve.sh
@@ -28,3 +28,28 @@ echo "Part 1: $result1"
 cd ../
 rm -rf solve
 
+mkdir solve
+cd solve
+split -l 3 ../input.txt
+for f in x*; do
+    var=$(cat $f | head -n 1)
+    for (( i=0; i<${#var}; i++ )); do
+        ch="${var:$i:1}"
+        res=$(cat $f | grep "$ch" | wc -l)
+        if [[ $res -eq 3 ]]; then
+            # echo "$ch"
+            ord=$(echo $ch | tr -d "\n" | od -An -t uC | bc)
+            if [ "$ord" -gt 96 ]; then
+                num=$((ord-96))
+            else
+                num=$((ord-38))
+            fi
+            echo "$num" >> result2
+            break
+        fi
+    done
+done
+result2=$(awk '{ sum += $1 } END { print sum }' result2)
+echo "Part 2: $result2"
+cd ../
+rm -rf solve
