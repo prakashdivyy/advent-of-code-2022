44759f914e63b94a6ba5eb43660340f5166f20e8
Prakash Divy <prakashdivyy@gmail.com>
2022-12-03 17:48:52 +0700

    Finish day 3 part 1

--- 03/solve.sh
+++ 03/solve.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+mkdir solve
+cd solve
+split -l 1 ../input.txt
+for f in x*; do
+    var=$(cat $f)
+    f1="${var:0:${#var}/2}"
+    f2="${var:${#var}/2}"
 
-var="vJrwpWtwJgWrhcsFMMfFFhFp"
-f1="${var:0:${#var}/2}"
-f2="${var:${#var}/2}"
-
-for (( i=0; i<${#f1}; i++ )); do
-    ch="${f1:$i:1}"
-    res=$(echo $f2 | grep "$ch")
-    if [[ $res ]]; then
-        echo "$ch"
-        ord=$(echo $ch | tr -d "\n" | od -An -t uC | bc)
-        if [ "$ord" -gt 96 ]; then
-            num=$((ord-96))
-        else
-            num=$((ord-38))
+    for (( i=0; i<${#f1}; i++ )); do
+        ch="${f1:$i:1}"
+        res=$(echo $f2 | grep "$ch")
+        if [[ $res ]]; then
+            # echo "$ch"
+            ord=$(echo $ch | tr -d "\n" | od -An -t uC | bc)
+            if [ "$ord" -gt 96 ]; then
+                num=$((ord-96))
+            else
+                num=$((ord-38))
+            fi
+            echo "$num" >> result
+            break
         fi
-        echo "$num"
-        break
-    fi
+    done
 done
+result1=$(awk '{ sum += $1 } END { print sum }' result)
+echo "Part 1: $result1"
+cd ../
+rm -rf solve
+
